c59d7c9fc9c2b0c2cb3ff582dfd11562819e4183
ricar <ricardosr4@gmail.com>
2024-05-15 00:00:56 -0400

    activity details ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,12 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.picasso:picasso:2.71828")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
