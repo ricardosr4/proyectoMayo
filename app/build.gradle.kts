3833d102148332fd6cc3a56c7efaf4f1e2080a1d
Marynuccy <tomas.marinucci06@gmail.com>
2024-05-16 17:19:47 -0300

    todolistonoarranca

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.lottie)
+    implementation(libs.compose.preview.renderer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
