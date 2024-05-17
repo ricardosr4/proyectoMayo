b3825212e3403c9516a69178dc0c95d659170164
Marynuccy <tomas.marinucci06@gmail.com>
2024-05-17 17:20:22 -0300

    grill_terminado

--- app/src/main/java/com/example/proyectomayo/ui/puppies/presenter/ActivityPuppies.kt
+++ app/src/main/java/com/example/proyectomayo/ui/puppies/presenter/ActivityPuppies.kt
@@ -2,6 +2,7 @@ package com.example.proyectomayo.ui.puppies.presenter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.proyectomayo.databinding.ActivityPuppiesBinding
 import com.example.proyectomayo.response.DogsResponse
 import com.example.proyectomayo.ui.puppies.adapter.Adapter
@@ -19,11 +20,27 @@ class ActivityPuppies : AppCompatActivity() {
     fun recyclerView(){
 
         val adapater = Adapter(getLista())
+        val gridLayoutManager = GridLayoutManager(this, 3)
+
+        binding.recylerPuppies.layoutManager = gridLayoutManager
         binding.recylerPuppies.adapter = adapater
 
     }
     fun getLista () :List<DogsResponse>{
         return listOf<DogsResponse>(
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
+            DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"),
             DogsResponse(image =  "https://images.dog.ceo/breeds/airedale/n02096051_7077.jpg"))
             }
 
