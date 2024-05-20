a4249d3d9be7f60a383fa18b8903f286502f3430
ricar <ricardosr4@gmail.com>
2024-05-20 10:11:11 -0400

    recyclers ok

--- app/src/main/java/com/example/proyectomayo/ui/us/presenter/ActivityUs.kt
+++ app/src/main/java/com/example/proyectomayo/ui/us/presenter/ActivityUs.kt
@@ -2,8 +2,12 @@ package com.example.proyectomayo.ui.us.presenter
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectomayo.data.service.response.local.provider.MemberProvider
+import com.example.proyectomayo.data.service.response.local.provider.UsProvider
 import com.example.proyectomayo.databinding.ActivityUsBinding
 import com.example.proyectomayo.ui.home.presenter.ActivityHome
+import com.example.proyectomayo.ui.us.adapter.MembersAdapter
+import com.example.proyectomayo.ui.us.adapter.UsAdapter
 
 class ActivityUs : AppCompatActivity() {
     private lateinit var binding: ActivityUsBinding
@@ -12,10 +16,29 @@ class ActivityUs : AppCompatActivity() {
         binding = ActivityUsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initRecyclerUs()
+        initRecyclerMembers()
+
+
         binding.ivBack.setOnClickListener{ navigateToHome()}
+
+
+
+
+
+
+    }
+    fun initRecyclerUs(){
+        val usAdapter = UsAdapter(UsProvider.getImageUs())
+        binding.recyclerUs.adapter = usAdapter
+    }
+    fun initRecyclerMembers(){
+        val membersAdapter = MembersAdapter(MemberProvider.getMember())
+        binding.recyclerMembers.adapter = membersAdapter
     }
      fun navigateToHome(){
         val intent = Intent(this, ActivityHome::class.java)
          startActivity(intent)
     }
+
 }
